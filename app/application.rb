fd1cf24981238fff47b257cbf79f464b4981ea6f
Sushi Chanrai <sushi@pathworks.co>
2018-08-29 14:19:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
+      binding.pry
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
